732f9248d8a43dd4da2a4ec01c19dbbdcfdbfc87
adho <adhoadhigal>
2019-03-05 15:53:59 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@
 
 var firstPlayer = "player1";
 var secondPlayer = "player2";
+var player1name = "";
+var player2name = "";
 
 var dice = function() {
-    var numbers = [1,2,3,4,5,6]
+    var numbers = [1,2,3,4,5,6];
     return Math.floor(Math.random() * numbers.length) + 1;
 }
 function reset() {
@@ -12,7 +14,7 @@ function reset() {
 }
 
 function Players() { 
-    this.playerName;                                                                                                                                                                                                                                                
+    this.playerName;                                        
     this.diceRoll = 0;
     this.roundTotal=0;
     this.totalScore=0;
@@ -22,10 +24,7 @@ Players.prototype.empty = function() {
     this.diceRoll=0;
 
 }
-
-
 Players.prototype.switch = function() {
-
 }
 
 Players.prototype.roll= function() {
@@ -39,30 +38,23 @@ Players.prototype.roll= function() {
 Players.prototype.hold = function() {
     this.totalScore += this.roundTotal;
     this.roundTotal= 0;
-    alert(this.playerName + "Please give the next chance to the next player.")
 }
-
 
 //user  interface//
 
 $(document).ready(function() {
-    $("#play").click(function() {
+    $("form").submit(function(event) {
+     event.preventDefault();
      firstPlayer = new Players();
      secondPlayer = new Players();
+  $()
      $(".content").show();
      $("#new-player").hide();
-     $("#play").hide();
-     $(".resume").hide();
-     
-    $(".new-player").submit(function(event){
-        event.preventDefault();
-        var player1name=$("#first-player").val();
-        $("#player1name").text(player1name);
+    player1name=$("#first-player").val();
+    $("#player1name").text(player1name);
 
-        var player2name=$("#second-player").val();
-        $("#player2name").text(player2name);
-    })
-    
+    player2name=$("#second-player").val();
+    $("#player2name").text(player2name);
 
     firstPlayer.playername = player1name;
     secondPlayer.playername = player2name;
@@ -75,15 +67,45 @@ $(document).ready(function() {
         firstPlayer.roll();
         $("#total-roll-1").text(firstPlayer.roundTotal);
     });
+    
     $("button#hold").click(function() {
-        firstPlayer.roll();
         firstPlayer.hold();
-        $("#total-1").text(firstplayer.totalScore);
-        $("total-roll-1").empty();
-        $("#roll-1").empty();
-
+        if (firstPlayer.totalScore >=100) {
+            alert(player1name + " is the winner");
+        } else {
+            alert("Please give the next chance to the next player.");
+            $("#total-1").text(firstPlayer.totalScore);
+            $("total-roll-1").empty();
+            $("#roll-1").empty();
+        }
     });
-  })
+   
+    $("button#rolling2").click(function() {
+        secondPlayer.diceRoll = dice();
+        $("#roll-2").text(secondPlayer.diceRoll);
+        secondPlayer.roll();
+        $("#total-roll-2").text(secondPlayer.roundTotal);
+    });
+
+    $("button#hold2").click(function() {
+        secondPlayer.hold();
+        if (secondPlayer.totalScore >=100) {
+            alert(player2name + " is the winner");
+        } else {
+            alert("Please give the next chance to the next player.");
+            $("#total-2").text(secondPlayer.totalScore);
+            $("total-roll-2").empty();
+            $("#roll-2").empty();
+        }
+    });
+
+  });
+  $('#list1').hide();
+
+  $('#instruction').click(function(){
+      $('#list1').fadeToggle(100);
+  });
+
 
     
 
